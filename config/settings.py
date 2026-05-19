5b5a1a39ab08c60d0cc74617e4d33c54e1a8ad8c
Raju Chandra <rajuchandra9466@gmail.com>
2026-05-19 10:04:15 +0530

    fixed env database

--- config/settings.py
+++ config/settings.py
@@ -80,20 +80,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-
     'default': dj_database_url.parse(
-
-        os.environ.get(
-            'DATABASE_URL',
-            'sqlite:///db.sqlite3'
-        )
-
-    )
-
-}
+        os.environ.get("DATABASE_URL")
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
