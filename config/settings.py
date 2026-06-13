6790c417ec09bd7f34a066379d2cad42f1bddaf4
Raju Chandra <rajuchandra9466@gmail.com>
2026-06-13 08:14:00 +0530

    Fix login redirect

--- config/settings.py
+++ config/settings.py
@@ -122,5 +122,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
